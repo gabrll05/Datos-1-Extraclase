d20a71bdb64b6220ffedc33dfc26c4f0ba5c9096
emarojass05 <142690430+emarojass05@users.noreply.github.com>
2023-08-25 21:23:02 -0600

    Update servidor Final

--- ERojas_GFernandez - Copy/src/socketproject/leo/servidor.java
+++ ERojas_GFernandez - Copy/src/socketproject/leo/servidor.java
@@ -50,5 +50,61 @@ class server extends JFrame implements Runnable {
 
 
 
+    @Override
+    public void run() { //Mediante esta clase se recibe la informacion enviada desde el socket y se almacena dentro del servidor
+
+        //System.out.println("Estoy escuchando");
+
+        try {
+            ServerSocket servidor=new ServerSocket(9999);
+
+            String ip,mensaje,nick;
+
+            sendpackage recievedpackage;
+
+            while(true) {
+
+                Socket misocket = servidor.accept();
+
+                ObjectInputStream data_package = new ObjectInputStream(misocket.getInputStream());
+
+                recievedpackage=(sendpackage) data_package.readObject();
+
+                ip=recievedpackage.getIp();
+                mensaje=recievedpackage.getMensaje();
+                nick=recievedpackage.getNick();
+
+                //DataInputStream flujo_entrada = new DataInputStream(misocket.getInputStream());
+                //String mensajede_texto = flujo_entrada.readUTF();
+
+                //areadetexto.append("\n" + mensajede_texto);
+
+                areadetexto.append(("\n" + nick +  ": " + mensaje + " para " +  ip));
+
+                Socket sendtoclient = new Socket(ip,9191); //Se crea un socket con la ip definoda y en el puerto 9990
+                ObjectOutputStream sendtocpackage = new ObjectOutputStream(sendtoclient.getOutputStream()); //Crea el flujo mediante el cual la informacion pasa atraves del socket
+
+                sendtocpackage.writeObject(recievedpackage);
+
+                sendtocpackage.close();
+
+                sendtoclient.close(); //Se cierra el socket
+                misocket.close();// Se cierra el socket
+            }
+
+        } catch (IOException e) { //Toma cualquier error o excepcion
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static JTextArea areadetexto = new JTextArea(); //Crea una variable privada  estatica
+
+    static{
+        areadetexto.setEditable(false);
+    }
 
 
+
+}
