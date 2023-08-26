f6050507866a2b1ed5e2e4d7bfdb9be304f73572
emarojass05 <142690430+emarojass05@users.noreply.github.com>
2023-08-25 21:22:10 -0600

    Uptade Cliente Final

--- ERojas_GFernandez - Copy/src/socketproject/leo/cliente.java
+++ ERojas_GFernandez - Copy/src/socketproject/leo/cliente.java
@@ -60,5 +60,101 @@ class Laminaclientt extends JPanel implements Runnable { //Se crea la clase de l
         add(caja);
 
 
+        areadet = new JTextArea();  // Se crea un area de texto en la cual se escrubiran los mensajes
+        areadet.setBounds(40,60,320,240);  // Ajustado el tamaño para que sea más grande
+        JScrollPane scroll  = new JScrollPane(areadet); //Se crea una barra para bajar en el texto
+        scroll.setBounds(40,60,320,240);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); //Se establece que la barra este activada todo el tiempo
+        areadet.setEditable(false); //No permitir e que pueda ser editado el JTextArea
+        add(scroll);
+
+
+        Thread mihilo = new Thread(this); //Se crea un hilo de forma de que la interfaz este constantemente a la escucha de la informacion enviada por los sockets
+        mihilo.start();
+
+
+
+        setBackground(Color.GREEN);
+    }
+
+    @Override
+    public void run() { //Se crea un metodo run el cual es el curerpo del hilo y controla la informacion de entrada al socket
+        try{
+
+            ServerSocket servidor_cliente = new ServerSocket(9191); //Se crea un socket en el puerto establecido por el usuario
+
+            Socket clientee;
+
+            sendpackage recievedpackage;
+            while(true){
+                clientee=servidor_cliente.accept(); //Mientras la instancia anterior sea verdad, el clientee va a aceptar la informacion enviada desde el socket del servidor
+
+                ObjectInputStream entranceobject = new ObjectInputStream(clientee.getInputStream()); //El objeto entrance object entra dentro del clientee
+
+                recievedpackage= (sendpackage) entranceobject.readObject(); //El paquete de recibo se convierte en el packete enviado desde el servidor medisante el metodo .readObject, el cual lea la informacion dentro del paquete enviado
+
+                areadet.append("\n" + recievedpackage.getNick() + ": " +  recievedpackage.getMensaje() + " " + recievedpackage.getIp()); //Se pega la infromacion leida del recievedpackage entro del area de texto
+
+            }
+
+
+        }catch(Exception e){ //Toma las excepciones dentro del metodo try/catch
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private class EnviaTexto implements ActionListener { //Se crea una clase privada que implementa el action listener para poder tomar la informacion, almacenarla en el socket y enviarla al servidor
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            try {
+
+                Socket misocket = new Socket("127.0.0.1",9999); //Se destina una direccion Ip y un puerto en el wue el socket estara establecido
+                sendpackage datos = new sendpackage(); //se crea un paquetebde envio en el cual el socket almacenara la informacion
+
+                datos.setIp(areadeip.getText()); //Toma el texto el texto escrito en el area del ip
+                datos.setNick(areadenick.getText());//Toma el texto el texto escrito en el area del nick
+                datos.setMensaje(caja.getText()); //Toma el texto el texto escrito en el area del texto
+                ObjectOutputStream data_package = new ObjectOutputStream(misocket.getOutputStream());//Crea la variable data package como un OutputStream del socket creado anterirormente
+                data_package.writeObject(datos);
+
+
+
+                misocket.close(); //Se encarga de cerrar el socket
+                //DataOutputStream flujo_salida = new DataOutputStream(misocket.getOutputStream());
+                //flujo_salida.writeUTF(texto);
+                //flujo_salida.close();
+
+            } catch (IOException ex) { //Toma cualquier error o excepcion dentro del proceso anterior
+                JOptionPane.showMessageDialog(null,"Error al conectar al servidor"); //Envia un mensaje en clase de excepcion
+            }
+        }
+    }
+}
+
+class sendpackage implements Serializable { //Crea una clase para la informacion enviada atraves de los sockets en los cuadros de texto
+
+    private String ip,mensaje,nick;
+
+    public String getIp(){
+        return ip;
+    }
+
+    public void setIp(String ip){
+        this.ip=ip;
+    }
+
+    public String getNick(){return nick;}
+
+    public void setNick(String nick){this.nick=nick;}
+
+    public String getMensaje(){
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje){
+        this.mensaje=mensaje;
+    }
+}
 
 
